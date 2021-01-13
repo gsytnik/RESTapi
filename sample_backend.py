ccdf6448c5140fa9b8b3c2687f5183fc76788c8d
Gennady Sytnik <gsytnik@calpoly.edu>
2021-01-13 15:45:52 -0800

    final commit

--- sample_backend.py
+++ sample_backend.py
@@ -24,7 +24,7 @@ users = {
          'id' : 'ppp222', 
          'name': 'Mac',
          'job': 'Professor',
-      }, 
+      },
       {
          'id' : 'yat999', 
          'name': 'Dee',
@@ -38,7 +38,7 @@ users = {
    ]
 }
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
    if request.method == 'GET':
       search_username = request.args.get('name')
@@ -56,6 +56,13 @@ def get_users():
       #resp.status_code = 200 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
+   elif request.method == 'DELETE':
+      userToRemove = request.get_json()
+      users['users_list'].remove(userToRemove)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
 
 @app.route('/users/<id>')
 def get_user(id):
@@ -64,4 +71,26 @@ def get_user(id):
         if user['id'] == id:
            return user
       return ({})
+   return users
+
+@app.route('/jobs/<job>')
+def get_job(job):
+   if job :
+   	  subdict = {'users_list' : []}
+   	  for user in users['users_list']:
+   	  	if user['job'] == job:
+   	  		subdict['users_list'].append(user)
+   	  return subdict
+   	  return ({})
+   return users
+
+@app.route('/names/<name>')
+def get_name(name):
+   if name :
+   	  subdict = {'users_list' : []}
+   	  for user in users['users_list']:
+   	  	if user['name'] == name:
+   	  		subdict['users_list'].append(user)
+   	  return subdict
+   	  return ({})
    return users
